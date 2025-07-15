8837e4169173849b2b8f2f467390d95dd9b45877
subha1405 <129583454+subha1405@users.noreply.github.com>
2025-07-15 22:22:10 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -11,14 +11,20 @@ public class Armstrong
     public static void main(String args[])
     {
         Scanner in=new Scanner(System.in);
-        int a,n,num,s=0;
+        int n,num,count=0,b,s=0,a;
         System.out.println("Enter a number");
         n=in.nextInt();
         num=n;
+        b=n;
+        while(b>0)
+        {
+            count+=1;
+            b/=10;
+        }
         while(n>0)
         {
            a=n%10;
-           s=s+(a*a*a);
+           s+=Math.pow(a,count);
            n=n/10;
         }
         if(num==s)
