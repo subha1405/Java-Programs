9e930f6c54ab11bd6ce089c6492cca2d883619a2
subha1405 <129583454+subha1405@users.noreply.github.com>
2025-07-15 22:42:30 +0530

    Add files via upload

---
+++ Stringexample.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class String here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.*;
+public class Stringexample
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        String a;
+        System.out.println("Enter a word");
+        a=in.nextLine();
+        System.out.println("String Entered:"+a);
+    }
+}
---
+++ lengthofastring.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class characters here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.*;
+public class lengthofastring
+{
+     public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        String a;
+        System.out.println("Enter a word");
+        a=in.nextLine();
+        System.out.print(a.length());
+    }
+}
